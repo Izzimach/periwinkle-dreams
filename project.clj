376d8cae32f72e422966ca364eda5a2cce17122f
Gary Haussmann <gary@interstellarechidna.com>
2014-12-18 15:09:06 -0700

    Basic client/server files

---
+++ project.clj
@@ -0,0 +1,53 @@
+(defproject org.clojars.haussman/periwinkle-dreams "0.1.0-SNAPSHOT"
+  :description "Sample app using om-react-pixi"
+  :url "https://github.com/Izzimach/periwinkle-dreams"
+  :license {:name "Apache 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2277"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [om "0.7.1"]
+                 [org.clojars.haussman/om-react-pixi "0.1.0-SNAPSHOT"]
+                 [prismatic/schema "0.2.4"]
+                 [prismatic/om-tools "0.3.6"]
+                 [ring/ring-core "1.2.2"]
+                 [ring/ring-jetty-adapter "1.2.2"]
+                 [compojure "1.3.1"]]
+
+  :source-paths ["src"]
+  :plugins [[lein-cljsbuild "1.0.3"]
+            [lein-ring "0.8.10"]]
+
+
+  :profiles {:dev {:dependencies [[figwheel "0.1.5-SNAPSHOT"]]
+                   :source-paths ["src" "dev-src"]
+                   :figwheel {
+                              :http-server-root "public"
+                              :server-port 8081
+                              :ring-handler periwinkle-dreams.server.core/handler
+                              }
+
+                   :plugins [[lein-figwheel "0.1.5-SNAPSHOT"]]}}
+
+  :ring {:handler periwinkle-dreams.server.core/handler :port 8081 }
+
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src/client"]
+                        :compiler {
+                                   :output-to "out/periwinkle-dreams.js"
+                                   :output-dir "out"
+                                   :optimizations :none
+                                   :source-map true}}
+                       {:id "packed"
+                        :source-paths ["src/client"]
+                        :compiler {
+                                   :output-to "out-min/periwinkle-dreams.min.js"
+                                   :output-dir "out-min"
+                                   :optimizations :advanced
+                                   :preamble ["react_pixi/react-pixi.min.js"]
+                                   :externs ["react_pixi/react-pixi.js"]
+                                   :closure-warnings {:externs-validation :off
+                                                      :non-standard-jsdoc :off}}}]})
+
---
+++ src/client/core.cljs
@@ -0,0 +1,72 @@
+(ns periwinkle-dreams.client.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [goog.events :as events]
+            [om.core :as om :include-macros true]
+            [schema.core :as schema]
+            [om-tools.core :as omtools :include-macros true]
+            [omreactpixi.abbrev :as pixi]
+            [clojure.string :as string]
+            [cljs.core.async :as async :refer [>! <! put!]]
+            [figwheel.client :as fw]))
+
+
+(defn assetpath [name] (str "../assets/" name))
+
+(enable-console-print!)
+
+(def flavormapping
+  {
+   :vanilla (assetpath "creamVanilla.png")
+   :chocolate (assetpath "creamChoco.png")
+   :mocha (assetpath "creamMocha.png")
+   :pink (assetpath "createmPink.png")
+   })
+
+(omtools/defcomponentk cupcakecomponent [[:data topping xposition] owner]
+  (render [_]
+          (let [creamimage (flavormapping topping)
+                cakeimage (assetpath "cupCake.png")
+                spritecenter (PIXI.Point. 0.5 0.5)]
+            (pixi/displayobjectcontainer {:x xposition, :y 100}
+                                         (pixi/sprite {:image creamimage :y -35 :anchor spritecenter :key "topping"})
+                                         (pixi/sprite {:image cakeimage :y 35 :anchor spritecenter :key "cake"}))))
+  (display-name [_] "Cupcake"))
+
+(omtools/defcomponentk cupcakestage [[:data width height xposition topping] owner]
+  (render [_]
+          (pixi/stage {:width width :height height}
+                      (pixi/tilingsprite {:image (assetpath "bg_castle.png") :width width :height height :key 1})
+                      (om/build cupcakecomponent {:topping topping :xposition xposition :ref "cupcake" :key 2})
+                      (pixi/text {:text "Vector text" :x xposition :y 10 :style #js {:font "40px Times"} :anchor (PIXI.Point. 0.5 0) :key 3})
+                      (pixi/bitmaptext {:text "Bitmap text" :x xposition :y 180 :tint 16rff88ff88 :style {:font "40 Comic_Neue_Angular"} :key 4})))
+  (display-name [_] "CupcakeStage"))
+
+
+(defonce appstate (atom {:width 0 :height 0 :xposition 100 :topping :vanilla}))
+
+(defn startcupcakes []
+  (let [inset #(- % 16)
+        w (-> js/window .-innerWidth inset)
+        h (-> js/window .-innerHeight inset)]
+    (swap! appstate #(-> % (assoc :width w) (assoc :height h)))
+    (om/root cupcakestage appstate
+             {:target (.getElementById js/document "my-app")})))
+
+
+;; gotta load the bitmap font first or else pixi bombs out
+(defonce needtopreload (atom true))
+
+(defn preloadthenstart [startfunc]
+  (let [fontloader (PIXI.BitmapFontLoader. (assetpath "comic_neue_angular_bold.fnt"))]
+    (swap! needtopreload (fn [_] false))
+    (.on fontloader "loaded" startfunc)
+    (.load fontloader)))
+
+(if @needtopreload
+  (preloadthenstart startcupcakes)
+  (startcupcakes))
+
+;; enable dynamic reloading via figwheel
+(fw/watch-and-reload
+  :jsload-callback (fn [] (print "reloaded!")))
+
